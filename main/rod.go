94bf715f8d64b6a10d43ca87eea183b93022ccda
Jhonatan Morais <jhonatanvinicius@gmail.com>
2026-03-20 16:45:59 -0400

    feat: Add fishing line simulation experiment

---
+++ main/rod.go
@@ -0,0 +1,12 @@
+components {
+  id: "script"
+  component: "/main/rod.script"
+}
+components {
+  id: "sprite"
+  component: "/builtins/materials/sprite.material"
+  properties {
+    "texture": "/builtins/graphics/sprite.atlas"
+    "blend_mode": 0
+  }
+}
