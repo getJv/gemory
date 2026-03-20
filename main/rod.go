0f64dd28938ce2d1686b77fe1451a560f63cd5e0
Jhonatan Morais <jhonatanvinicius@gmail.com>
2026-03-20 17:03:14 -0400

    fix: Correct rod.go component structure by removing invalid sprite

--- main/rod.go
+++ main/rod.go
@@ -1,8 +1,15 @@
 components {
   id: "script"
   component: "/main/rod.script"
-}
-components {
-  id: "sprite"
-  component: "/builtins/materials/sprite.material"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
 }
